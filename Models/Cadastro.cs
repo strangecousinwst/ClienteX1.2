3f51526292dde68bbe926c5e7f2b451b9d90fdce
strangecousinwst <139879737+strangecousinwst@users.noreply.github.com>
2025-02-28 11:18:07 +0000

    batman

---
+++ Models/Cadastro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKX.Models
+{
+    public class Cadastro
+    {
+        public int Id { get; set; }
+
+        public DateTime Data { get; set; }
+
+        [Display(Name = "Descritivo")]
+        public string? Texto { get; set; }
+
+        // chave estrangeira para Cliente:
+        [Display(Name = "Cliente")]
+        public int ClienteId { get; set; }
+
+    }
+}
